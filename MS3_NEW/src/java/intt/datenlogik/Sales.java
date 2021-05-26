6af78e992cdfe8a59570cf176886c2c8e9115d89
Marius Bader <marius.bader@stud.hs-kempten.de>
2021-05-26 17:30:23 +0200

    MS3_NEW alle Cockpitfunktionen klappen

---
+++ MS3_NEW/src/java/intt/datenlogik/Sales.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intt.datenlogik;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author inttentwickler
+ */
+@Entity
+@Table(name = "sales")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Sales.findAll", query = "SELECT s FROM Sales s"),
+    @NamedQuery(name = "Sales.findBySaleID", query = "SELECT s FROM Sales s WHERE s.saleID = :saleID"),
+    @NamedQuery(name = "Sales.findByAmount", query = "SELECT s FROM Sales s WHERE s.amount = :amount"),
+    @NamedQuery(name = "Sales.findByTimestamp", query = "SELECT s FROM Sales s WHERE s.timestamp = :timestamp")})
+public class Sales implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "saleID")
+    private Integer saleID;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "amount")
+    private int amount;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+    @JoinColumn(name = "stationID", referencedColumnName = "stationID")
+    @ManyToOne(optional = false)
+    private Station stationID;
+    @JoinColumn(name = "productID", referencedColumnName = "productID")
+    @ManyToOne(optional = false)
+    private Products productID;
+    @JoinColumn(name = "sellerID", referencedColumnName = "sellerID")
+    @ManyToOne(optional = false)
+    private Seller sellerID;
+
+    public Sales() {
+    }
+
+    public Sales(Integer saleID) {
+        this.saleID = saleID;
+    }
+
+    public Sales(Integer saleID, int amount, Date timestamp) {
+        this.saleID = saleID;
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+
+    public Integer getSaleID() {
+        return saleID;
+    }
+
+    public void setSaleID(Integer saleID) {
+        this.saleID = saleID;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Station getStationID() {
+        return stationID;
+    }
+
+    public void setStationID(Station stationID) {
+        this.stationID = stationID;
+    }
+
+    public Products getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Products productID) {
+        this.productID = productID;
+    }
+
+    public Seller getSellerID() {
+        return sellerID;
+    }
+
+    public void setSellerID(Seller sellerID) {
+        this.sellerID = sellerID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (saleID != null ? saleID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Sales)) {
+            return false;
+        }
+        Sales other = (Sales) object;
+        if ((this.saleID == null && other.saleID != null) || (this.saleID != null && !this.saleID.equals(other.saleID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "intt.datenlogik.Sales[ saleID=" + saleID + " ]";
+    }
+    
+}
